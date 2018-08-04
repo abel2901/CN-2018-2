abf24527ac5bb86e433d6e9f0b042ddf660692ed
Anderson Vieira do Nascimento <34370927+andersonvieiradonascimento@users.noreply.github.com>
2018-08-04 11:34:50 -0300

    STEWART II, p 636

---
+++ Recorrencias/stewartp636.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  2 21:37:18 2018
+
+@author: anderson v. do nascimento
+"""
+
+import matplotlib.pyplot as plt
+
+def recur_tool(n):
+    if n == 1:
+        return 2
+    else:
+        return float((0.5)*(recur_tool(n-1) + 6))
+
+n = input("Até onde você quer determinar a sequência? ")
+
+sequence = []
+
+for i in range(int(n)):
+    sequence.append(recur_tool(i+1))
+    
+#print("Sequência: " + str(sequence))
+    
+plt.figure()
+plt.plot(sequence)
+plt.ion()
+plt.show()
